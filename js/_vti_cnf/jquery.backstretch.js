0b8d8accbd2222c3cd447238b3a348ad5c59fdd7
rjhealy <rjh32009@gmail.com>
2014-08-26 11:37:28 -0400

    Initial Commit
    pushing personal site

---
+++ js/_vti_cnf/jquery.backstretch.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|JOEHEALY94FB\\joehealy
+vti_modifiedby:SR|JOEHEALY94FB\\joehealy
+vti_timelastmodified:TR|25 Aug 2014 23:44:26 -0000
+vti_timecreated:TR|25 Aug 2014 23:44:26 -0000
+vti_cacheddtm:TX|25 Aug 2014 23:44:25 -0000
+vti_filesize:IR|11734
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
